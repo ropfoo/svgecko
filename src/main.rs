c3ed1647c21eac4f2c7b13fb1e93c080c347da7f
RopFoo <ropfoo@gmail.com>
2023-06-04 21:01:26 +0200

    node tree types

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,61 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Ok, Result};
 use std::fs;
+
+#[derive(Debug)]
+struct Svg {
+    width: i32,
+    height: i32,
+}
+
+#[derive(Debug)]
+enum Element {
+    Svg(Svg),
+}
+
+impl Element {
+    fn name(&self) -> &str {
+        match self {
+            Element::Svg(_) => "svg",
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    element: Element,
+    children: Option<Vec<Node>>,
+}
+
+fn create_parts(s: &str) {
+    let parts = s.split_terminator("<");
+    let mut elements: Vec<Node> = vec![];
+    parts.enumerate().for_each(|(idx, part)| {
+        if idx == 1 {
+            elements.push(Node {
+                element: Element::Svg(Svg {
+                    width: 10,
+                    height: 10,
+                }),
+                children: None,
+            })
+        }
+
+        if part.ends_with("/>") {}
+    });
+    println!("{:?}", elements)
+}
 
 fn read_file(path: &str) -> Result<String> {
     let file = fs::read(path).context("Failed to load file on path")?;
-    return String::from_utf8(file).context("Failed tp parse file");
+    let content = String::from_utf8(file)?;
+    return Ok(content);
 }
 
 fn main() {
     let svg_file = read_file("icons/search-icon.svg");
-    match svg_file {
-        Err(e) => println!("{}", e),
-        Ok(result) => println!("{:?}", result),
+    if svg_file.is_err() {
+        return;
     }
+    let svg_string = svg_file.unwrap();
+    create_parts(&svg_string);
 }
