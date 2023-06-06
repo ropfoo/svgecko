7b97f3f6293aebe5caca4b4df2a51888cfdbf726
RopFoo <ropfoo@gmail.com>
2023-06-06 07:13:07 +0200

    separate attributes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
 use anyhow::{Context, Ok, Result};
-use std::fs;
+use std::{fs, process::id, str::FromStr};
+
+fn split_attributes(s: &str, element_tag: &str) {
+    let parts = s
+        .replace(element_tag, "")
+        .replace(">", "")
+        .split_terminator("\" ")
+        .filter(|&p| p.contains("="))
+        .for_each(|p| {
+            let thing = p.replace("\"", "");
+            let res = thing.split_once("=");
+            if res.is_some() {
+                let (key, value) = res.unwrap();
+                println!("key:{} ,value {}", key, value);
+            }
+        });
+}
 
 #[derive(Debug)]
 struct Svg {
     width: i32,
     height: i32,
+}
+
+impl FromStr for Svg {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        split_attributes(s, "svg");
+        // println!("{:?}", parts);
+        return Ok(Svg {
+            width: 0,
+            height: 1,
+        });
+    }
 }
 
 #[derive(Debug)]
@@ -31,6 +60,9 @@ fn create_parts(s: &str) {
     let mut elements: Vec<Node> = vec![];
     parts.enumerate().for_each(|(idx, part)| {
         if idx == 1 {
+            let attribute_name = part.split_whitespace().next().unwrap_or("");
+            // TODO handle get by attribute
+            Svg::from_str(part);
             elements.push(Node {
                 element: Element::Svg(Svg {
                     width: 10,
